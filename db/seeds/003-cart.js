48867cbd134d4d15fb5086f47584fcf24d848218
Reito0320 <reito@codechrysalis.io>
2026-02-20 14:19:31 +0900

    change seedsファイルの修正

--- db/seeds/003-cart.js
+++ db/seeds/003-cart.js
@@ -4,6 +4,7 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
+  await knex('order').del();
   await knex('cart').del();
-  await knex('cart').insert([{ user_id: 3 }, { user_id: 2 }, { user_id: 1 }]);
+  await knex('cart').insert([{ user_id: 1 }, { user_id: 2 }, { user_id: 3 }]);
 };
