d5911a6e973a387941aa19b2175b2db08059f0f8
Reito0320 <reito@codechrysalis.io>
2026-02-20 13:20:54 +0900

    change scriptの修正

--- src/utills/knex.js
+++ src/utills/knex.js
@@ -1,7 +1,7 @@
 const status = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
 const config = require('../../knexfile.js')[status];
 
 const knex = require('knex')(config);
-console.log(config);
 
 module.exports = knex;
